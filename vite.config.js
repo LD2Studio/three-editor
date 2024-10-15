ebd7d424ce91ed99902e6d2ffc1ed3318b7a6bad
Laurent Dethoor <ld2studiogame@gmail.com>
2024-10-15 16:38:56 +0200

    Change code for Vite bundler

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+export default {
+    root: 'src',
+    base: './',
+    build: {
+        outDir: '../dist',
+        emptyOutDir: false,
+        sourcemap: true,
+    },
+
+    server : {
+        port: 3000,
+        host: true
+    }
+}
